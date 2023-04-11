5318e6a87f327322e0cd26ea712c4e41d3fa320e
Jonas Gao <gaopinsong@live.cn>
2023-04-11 13:43:30 +0800

    Update restore.ps1, remove unused function, update dependency install

--- restore.ps1
+++ restore.ps1
@@ -1,10 +1,8 @@
 param(
-  [switch]$Install,
   [switch]$Packer,
-  [switch]$Config,
   [switch]$Dependency,
   [switch]$BuildFzf,
-  $Proxy
+  $HttpProxy
 )
 
 function Install-Packer
@@ -17,45 +15,52 @@ function Install-Packer
   git clone https://github.com/wbthomason/packer.nvim $D
 }
 
-function Restore-Config
-{
-  $CONF_HOME="$HOME/AppData/Local/nvim"
-  if (Test-Path $CONF_HOME)
-  {
-    Write-Host -ForegroundColor Yellow "`"$CONF_HOME`" Already exists!"
-  } else
-  {
-    git clone "git@github.com:jonasgao/nvimrc.git" $CONF_HOME
-    Write-Host -ForegroundColor Green "Restore neovim config files finished."
-  }
-}
-
 function Install-Dependency
 {
-  if ($Proxy) {
-    Write-Host "Config scoop using proxy: $Proxy"
-    scoop config proxy $Proxy
+  if ($HttpProxy)
+  {
+    Write-Host "Config scoop using proxy: http://$HttpProxy"
+    scoop config proxy $HttpProxy
   }
-  # For telescope-fzf-native
-  scoop install cmake
-  # for telescope preview
-  scoop install bat
-  # for fuzzy finder and live-grep (like telescope and fzf ...)
-  scoop install ripgrep
-  scoop install fd
+  try
+  {
+    # For telescope-fzf-native
+    scoop install cmake
+    # for telescope preview
+    scoop install bat
+    # for fuzzy finder and live-grep (like telescope and fzf ...)
+    scoop install ripgrep
+    scoop install fd
 
-  # For treesitter
-  scoop install gcc
-  # npm install -g tree-sitter-cli
-  # if download fail, support HTTPS_PROXY env
-  # eg: (Powershell) $HTTPS_PROXY = "http://127.0.0.1:7891"; npm install -g tree-sitter-cli
-  # or
-  # cargo install tree-sitter-cli
-  # install cargo please download https://win.rustup.rs/x86_64
-  # linux or unix, use "curl https://sh.rustup.rs -sSf | sh"
+    # For treesitter
+    scoop install gcc
+    # install tree-sitter-cli
+    if ($HttpProxy)
+    {
+      $HTTPS_PROXY = "http://$HttpProxy"
+    }
+    try
+    {
+      npm install -g tree-sitter-cli
+    } finally
+    {
+      Remove-Variable HTTPS_PROXY
+    }
+    # or
+    # cargo install tree-sitter-cli
+    # install cargo please download https://win.rustup.rs/x86_64
+    # linux or unix, use "curl https://sh.rustup.rs -sSf | sh"
 
-  # For lsp
-  scoop install lua-language-server
+    # For lsp
+    scoop install lua-language-server
+  } finally
+  {
+    if ($HttpProxy)
+    {
+      Write-Host "Remove scoop proxy"
+      scoop config rm proxy
+    }
+  }
 }
 
 function Build-TelescopeFzfNative
@@ -64,33 +69,14 @@ function Build-TelescopeFzfNative
   cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build
 }
 
-function Get-Nvim
-{
-  $NvimMsi = "$HOME\Downloads\nvim-win64.msi"
-  Write-Output "Downloading msi"
-  Invoke-RestMethod -Uri "https://github.com/neovim/neovim/releases/download/stable/nvim-win64.msi" -OutFile $NvimMsi -Proxy $Proxy
-  Write-Output "Downloaded"
-  Start-Process $NvimMsi
-}
-
 if ($Packer)
 {
   Install-Packer
 }
 
-if ($Config)
-{
-  Restore-Config
-}
-
 if ($Dependency)
 {
   Install-Dependency
-}
-
-if ($Install)
-{
-  Get-Nvim
 }
 
 if ($BuildFzf)
