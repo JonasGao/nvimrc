947837edeb56d929f98c286a9af844a6dc88aa21
Jonas Gao <gaopinsong@live.cn>
2023-08-30 16:13:34 +0800

    Update mason config

--- plugin/0_mason.lua
+++ plugin/0_mason.lua
@@ -1,8 +1,8 @@
-local ready, mason = pcall(require, 'mason')
-if (not ready) then return end
+local mason_ready, mason = pcall(require, 'mason')
+if (not mason_ready) then return end
 
-local ready, mason_lspconfig = pcall(require, 'mason-lspconfig')
-if (not ready) then return end
+local mason_lspconfig_ready, mason_lspconfig = pcall(require, 'mason-lspconfig')
+if (not mason_lspconfig_ready) then return end
 
 mason.setup()
 mason_lspconfig.setup()
