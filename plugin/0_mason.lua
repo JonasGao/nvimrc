8a34f4eb9547d9837b35b0107c17bf0f6b861b59
Jonas Gao <gaopinsong@live.cn>
2023-09-16 16:38:52 +0800

    Config mason use proxy

--- plugin/0_mason.lua
+++ plugin/0_mason.lua
@@ -4,5 +4,9 @@ if (not mason_ready) then return end
 local mason_lspconfig_ready, mason_lspconfig = pcall(require, 'mason-lspconfig')
 if (not mason_lspconfig_ready) then return end
 
-mason.setup()
+mason.setup({
+  github = {
+    download_url_template = "https://ghps.cc/https://github.com/%s/releases/download/%s/%s"
+  }
+})
 mason_lspconfig.setup()
