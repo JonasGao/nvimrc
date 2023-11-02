de38bd6dcb7cfd1300b2f34f5f12adfd42dde8fd
Jonas Gao <gaopinsong@live.cn>
2023-11-02 12:42:11 +0800

    Update install.ps1.

--- install.ps1
+++ install.ps1
@@ -1,8 +1,5 @@
 param(
-  [switch]$Packer,
-  [switch]$Dependency,
-  [switch]$BuildFzf,
-  [string]$HttpProxy
+  [switch]$BuildFzf
 )
 
 function Install-Packer
@@ -10,9 +7,11 @@ function Install-Packer
   $D = "$env:LOCALAPPDATA\nvim-data\site\pack\packer\start\packer.nvim"
   if (Test-Path $D)
   {
+    Write-Output "'Packer' is already exists"
     return
   }
   git clone https://github.com/wbthomason/packer.nvim $D
+  Write-Output "'Packer' was cloned successfully"
 }
 
 function Install-Dependency
@@ -23,73 +22,47 @@ function Install-Dependency
     Set-ExecutionPolicy RemoteSigned -Scope CurrentUser
     Invoke-RestMethod get.scoop.sh | Invoke-Expression
   }
-  if ($HttpProxy)
-  {
-    Write-Host "Config scoop using proxy: http://$HttpProxy"
-    scoop config proxy $HttpProxy
-  }
-  try
-  {
-    # For telescope-fzf-native
-    scoop install cmake
-    # for telescope preview
-    scoop install bat
-    # for fuzzy finder and live-grep (like telescope and fzf ...)
-    scoop install ripgrep
-    scoop install fd
+  # For telescope-fzf-native
+  scoop install cmake
+  # for telescope preview
+  scoop install bat
+  # for fuzzy finder and live-grep (like telescope and fzf ...)
+  scoop install ripgrep
+  scoop install fd
 
-    # For treesitter
-    scoop install gcc
-    # install tree-sitter-cli
-    if ($HttpProxy)
-    {
-      $HTTPS_PROXY = "http://$HttpProxy"
-    }
-    try
-    {
-      if (-not (Get-Command npm))
-      {
-        scoop install nodejs-lts
-      }
-      npm install -g tree-sitter-cli
-    } finally
-    {
-      Remove-Variable HTTPS_PROXY
-    }
-    # or
-    # cargo install tree-sitter-cli
-    # install cargo please download https://win.rustup.rs/x86_64
-    # linux or unix, use "curl https://sh.rustup.rs -sSf | sh"
-
-    # For lsp
-    scoop install lua-language-server
-  } finally
+  # For treesitter
+  scoop install gcc
+  # install tree-sitter-cli
+  try {
+    Get-Command npm
+  } catch 
   {
-    if ($HttpProxy)
-    {
-      Write-Host "Remove scoop proxy"
-      scoop config rm proxy
-    }
+    scoop install nodejs-lts
   }
+  npm install -g tree-sitter-cli
+  # or
+  # cargo install tree-sitter-cli
+  # install cargo please download https://win.rustup.rs/x86_64
+  # linux or unix, use "curl https://sh.rustup.rs -sSf | sh"
 }
 
 function Build-TelescopeFzfNative
 {
-  Set-Location "$env:LOCALAPPDATA\nvim-data\site\pack\packer\start\telescope-fzf-native.nvim\"
-  cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build
-}
 
-if ($Packer)
-{
-  Install-Packer
-}
-
-if ($Dependency)
-{
-  Install-Dependency
+  Push-Location "$env:LOCALAPPDATA\nvim-data\site\pack\packer\start\telescope-fzf-native.nvim\"
+  try {
+    cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build
+  } finally
+  {
+    Pop-Location
+  }
 }
 
 if ($BuildFzf)
 {
   Build-TelescopeFzfNative
+  return
 }
+
+Install-Packer
+Install-Dependency
