e261b0ef053f95a33e723b705b05c953bb5f9ff8
Jonas Gao <gaopinsong@live.cn>
2023-11-04 20:24:17 +0800

    Mapping key with lua.

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -1,45 +1,4 @@
---[[
-""" Basic mapping
-nnoremap  dw  vb"_d
-
-""" Leaders mapping
-let mapleader="\\"
-nnoremap  <Leader>Ss  :source $MYVIMRC<CR>
-" Switch buffer
-nnoremap  <Leader>h   :bp<CR>
-nnoremap  <Leader>l   :bn<CR>
-nnoremap  <Leader>bl  :ls<CR>
-nnoremap  <Leader>bd  :bd<CR>
-nnoremap  <Leader>1   :b1<CR>
-nnoremap  <Leader>2   :b2<CR>
-nnoremap  <Leader>3   :b3<CR>
-nnoremap  <Leader>4   :b4<CR>
-nnoremap  <Leader>5   :b5<CR>
-nnoremap  <Leader>6   :b6<CR>
-nnoremap  <Leader>7   :b7<CR>
-nnoremap  <Leader>8   :b8<CR>
-nnoremap  <Leader>9   :b9<CR>
-" Split
-nnoremap  <Leader>ss  :split<Return><C-w>w
-nnoremap  <Leader>sv  :vsplit<Return><C-w>w
-" Resize
-nnoremap  <Leader>W,  <C-w><
-nnoremap  <Leader>W.  <C-w>>
-" Switch foldmethod
-nnoremap	<Leader>zs	:set foldmethod=syntax<CR>
-nnoremap	<Leader>zi	:set foldmethod=indent<CR>
-nnoremap	<Leader>zm	:set foldmethod=manual<CR>
-nnoremap	<Leader>zk	:set foldmethod=marker<CR>
-nnoremap	<Leader>Z	  :set nofoldenable<CR>
-" Insert current date
-nnoremap	<Leader>id  :r!date<CR>
-inoremap	<C-p>d
-
-]]
---
-
 -- Inspired by https://github.com/brainfucksec/neovim-lua/blob/main/nvim/lua/core/keymaps.lua
-
 local function map(mode, key, cmd, desc)
   local o = { noremap = true, silent = true, desc = desc }
   vim.keymap.set(mode, key, cmd, o)
@@ -51,11 +10,53 @@ map("i", "<C-p>d", "<C-r>=system('date')<CR>")
 
 local wk = require("which-key")
 wk.register({
-  y = { "*y", "Yank" },
-  p = { "*p", "Paste", mode = { "n", "v" } },
+  y = { "\"*y", "Yank" },
+  p = { "\"*p", "Paste", mode = { "n", "v" } },
   x = { "<cmd>set paste!<cr>", "Set paste mode" },
   r = { "<cmd>bro ol<cr>" },
-  n = { "<cmd>set nu! rnu!<cr>", "Switch line number" },
-  w = { "<cmd>set wrap!<cr>", "Switch wrap mode" },
+  i = {
+    name = "+Install",
+    d = { "<cmd>r!date<cr>", "+Date" },
+  },
+  n = {
+    name = "+Switch",
+    n = { "<cmd>set nu! rnu!<cr>", "Switch line number" },
+    w = { "<cmd>set wrap!<cr>", "Switch wrap mode" },
+  },
   m = { ":set filetype=", "Set filetype" },
+  S = { "<cmd>source $MYVIMRC<cr>", "Reload rcfile" },
+  w = {
+    name = "+Window",
+    [","] = { "<C-w><", "" },
+    ["."] = { "<C-w>>", "" },
+  },
+  s = {
+    name = "+Split",
+    s = { "<cmd>split<cr><C-w>w", "Split horizontal" },
+    v = { "<cmd>vsplit<cr><C-w>w", "Split vertical" },
+  },
+  b = {
+    name = "+Buffer",
+    b = { "<cmd>ls<cr>", "List" },
+    d = { "<cmd>bd<cr>", "Delete current" },
+    h = { "<cmd>bp<cr>", "Prev buffer" },
+    l = { "<cmd>bn<cr>", "Next buffer" },
+  },
+  z = {
+    name = "+FlodMethod",
+    z = { "<cmd>set nofoldenable<cr>", "Nofoldenable" },
+    s = { "<cmd>set foldmethod=syntax<cr>", "Syntax" },
+    i = { "<cmd>set foldmethod=indent<cr>", "Indent" },
+    m = { "<cmd>set foldmethod=manual<cr>", "Manual" },
+    k = { "<cmd>set foldmethod=marker<cr>", "Marker" },
+  },
+  ["1"] = { "<cmd>b1<cr>", "Buffer 1" },
+  ["2"] = { "<cmd>b2<cr>", "Buffer 2" },
+  ["3"] = { "<cmd>b3<cr>", "Buffer 3" },
+  ["4"] = { "<cmd>b4<cr>", "Buffer 4" },
+  ["5"] = { "<cmd>b5<cr>", "Buffer 5" },
+  ["6"] = { "<cmd>b6<cr>", "Buffer 6" },
+  ["7"] = { "<cmd>b7<cr>", "Buffer 7" },
+  ["8"] = { "<cmd>b8<cr>", "Buffer 8" },
+  ["9"] = { "<cmd>b9<cr>", "Buffer 9" },
 }, { prefix = "<leader>" })
