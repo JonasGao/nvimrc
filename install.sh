bfdef73dab28469ab397fe3a8e9ca138dc4c89f3
Jonas Gao <gaopinsong@live.cn>
2024-02-06 23:36:47 +0800

    Update depend tools install script

--- install.sh
+++ install.sh
@@ -37,11 +37,35 @@ install_packer() {
   git clone https://github.com/wbthomason/packer.nvim "$D"
 }
 
+cargo_install() {
+  REPO=$1; CMD=$2
+  [ -z "$GITHUB_HOME" ] && [ -d "$HOME/github" ] && GITHUB_HOME="$HOME/github"
+  [ -z "$GITHUB_HOME" ] && [ -d "$HOME/Github" ] && GITHUB_HOME="$HOME/Github"
+  [ -z "$GITHUB_HOME" ] && mkdir -p "$HOME/github" ] && GITHUB_HOME="$HOME/github"
+  if ! command -v "$CMD" &> /dev/null; then
+    echo "Install $CMD..."
+    pushd "$GITHUB_HOME" || exit 8
+    [ ! -d "$CMD" ] && git clone $REPO $CMD
+    pushd "$GITHUB_HOME/$CMD" || exit 8
+    cargo build --release
+    install target/release/$CMD /usr/local/bin
+    popd || exit 9
+    popd || exit 9
+  fi
+}
+
 install_dependencies() {
   #$PACKER install ripgrep cmake gcc bat fd
   #Case my mackbook is too old, homebrew not support install these tools
-  echo "Please manually install tools: ripgrep and fd, bat"
-  echo "Please install compile dependencies: cmake & gcc"
+  #echo "Please manually install tools: ripgrep and fd, bat"
+  #echo "Please install compile dependencies: cmake & gcc"
+  command -v cargo &> /dev/null \
+    || (echo -e "\033[31mInstall failure. Install ripgrep/fd/bat need rust&cargo.\033[0m" \
+    && echo -e "\033[33mInstall rust by \"curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh\"\033[0m" \
+    && exit 7)
+  cargo_install https://github.com/sharkdp/fd fd
+  cargo_install https://github.com/BurntSushi/ripgrep rg
+  command -v bat &> /dev/null || cargo install --locked bat #https://github.com/sharkdp/bat.git
 }
 
 build_telescope_fzf() {
