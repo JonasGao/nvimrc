b1a34603198516a3d953c631fb942affc6d45538
gaopinsong@live.cn <gaopinsong@live.cn>
2024-07-14 11:41:48 +0800

    Fixup mason proxy setting

--- plugin/0_mason.lua
+++ plugin/0_mason.lua
@@ -4,7 +4,7 @@ if (not mason_ready) then return end
 local mason_lspconfig_ready, mason_lspconfig = pcall(require, 'mason-lspconfig')
 if (not mason_lspconfig_ready) then return end
 
-local prefix = os.getenv("GH_RPOXY") or vim.api.nvim_get_var('gh_proxy') or ""
+local prefix = os.getenv("GH_RPOXY") or vim.g.gh_proxy or ""
 local url_template = prefix .. "https://github.com/%s/releases/download/%s/%s"
 mason.setup({
   github = {
