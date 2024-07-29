24b72f7c062095851858894851a84d2a4a22ddb3
Jonas Gao <gaopinsong@live.cn>
2024-07-29 11:41:35 +0800

    Add snipe

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -20,6 +20,16 @@ return {
     },
   },
 
+  -- Buffer navigation
+  {
+    "leath-dub/snipe.nvim",
+    config = function()
+      local snipe = require("snipe")
+      snipe.setup()
+      vim.keymap.set("n", "gb", snipe.create_buffer_menu_toggler())
+    end
+  },
+
   -- LSP and Diagnostic
   { 'williamboman/mason.nvim' },
   { 'williamboman/mason-lspconfig.nvim' },
