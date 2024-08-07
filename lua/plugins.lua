af8119518d084f800e9e7ea8b3e24cb7b4f82394
JonasGao <gaopinsong@live.cn>
2024-08-07 14:24:41 +0800

    Update snipe config.

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -23,11 +23,9 @@ return {
   -- Buffer navigation
   {
     "leath-dub/snipe.nvim",
-    config = function()
-      local snipe = require("snipe")
-      snipe.setup()
-      vim.keymap.set("n", "gb", snipe.create_buffer_menu_toggler())
-    end
+    keys = {
+      {"gb", function () require("snipe").open_buffer_menu() end, desc = "Open Snipe buffer menu"}
+    },
   },
 
   -- LSP and Diagnostic
