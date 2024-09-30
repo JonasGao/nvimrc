bcef7686441634f1e0d356232a229a5b1daee1f6
Jonas Gao <gaopinsong@live.cn>
2024-09-30 12:28:37 +0800

    Update plugins

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,104 +1,116 @@
 return {
 
-	-- Improvements UI
-	{ "stevearc/dressing.nvim" },
+  -- Improvements UI
+  { "stevearc/dressing.nvim" },
 
-	-- Notification
-	{ "MunifTanjim/nui.nvim" },
-	{ "rcarriga/nvim-notify" },
-	{ "folke/noice.nvim" },
+  -- Notification
+  { "MunifTanjim/nui.nvim" },
+  { "rcarriga/nvim-notify" },
+  { "folke/noice.nvim" },
 
-	-- Which key
-	{ "folke/which-key.nvim" },
+  -- Which key
+  { "folke/which-key.nvim" },
 
-	-- File explorer
-	{
-		"nvim-tree/nvim-tree.lua",
-		dependencies = {
-			"nvim-tree/nvim-web-devicons",
-			"b0o/nvim-tree-preview.lua",
-		},
-	},
+  -- File explorer
+  {
+    "nvim-tree/nvim-tree.lua",
+    dependencies = {
+      "nvim-tree/nvim-web-devicons",
+      "b0o/nvim-tree-preview.lua",
+    },
+  },
 
-	-- Buffer navigation
-	{
-		"leath-dub/snipe.nvim",
-		keys = {
-			{
-				"gb",
-				function()
-					require("snipe").open_buffer_menu()
-				end,
-				desc = "Open Snipe buffer menu",
-			},
-		},
-	},
+  -- Buffer navigation
+  {
+    "leath-dub/snipe.nvim",
+    keys = {
+      {
+        "gb",
+        function()
+          require("snipe").open_buffer_menu()
+        end,
+        desc = "Open Snipe buffer menu",
+      },
+    },
+  },
 
-	-- LSP and Diagnostic
-	{ "williamboman/mason.nvim" },
-	{ "williamboman/mason-lspconfig.nvim" },
-	{ "neovim/nvim-lspconfig" },
-	{ "j-hui/fidget.nvim" },
-	{ "mhartington/formatter.nvim" },
-	{ "folke/trouble.nvim", dependencies = "kyazdani42/nvim-web-devicons" },
-	{ "glepnir/lspsaga.nvim", dependencies = "nvim-tree/nvim-web-devicons" },
+  -- LSP and Diagnostic
+  { "williamboman/mason.nvim" },
+  { "williamboman/mason-lspconfig.nvim" },
+  { "neovim/nvim-lspconfig" },
+  { "j-hui/fidget.nvim" },
+  { "mhartington/formatter.nvim" },
+  { "folke/trouble.nvim",                  dependencies = "kyazdani42/nvim-web-devicons" },
+  { "glepnir/lspsaga.nvim",                dependencies = "nvim-tree/nvim-web-devicons" },
 
-	-- Colorschema
-	{ "Tsuzat/NeoSolarized.nvim" },
+  -- Colorschema
+  { "Tsuzat/NeoSolarized.nvim" },
 
-	-- Statusline
-	{ "hoob3rt/lualine.nvim", dependencies = "kyazdani42/nvim-web-devicons" },
+  -- Statusline
+  { "hoob3rt/lualine.nvim",                dependencies = "kyazdani42/nvim-web-devicons" },
 
-	-- Easy motion
-	{ "easymotion/vim-easymotion" },
+  -- Easy motion
+  { "easymotion/vim-easymotion" },
 
-	-- Visual Multi
-	{ "mg979/vim-visual-multi", lazy = false },
+  -- Visual Multi
+  { "mg979/vim-visual-multi",              lazy = false },
 
-	-- Autocompletion
-	{ "onsails/lspkind-nvim" },
-	{ "hrsh7th/nvim-cmp" },
-	{ "hrsh7th/cmp-nvim-lsp" },
-	{ "hrsh7th/cmp-buffer" },
-	{ "saadparwaiz1/cmp_luasnip" }, -- Snippets source for nvim-cmp
-	{ "L3MON4D3/LuaSnip" }, -- Snippets plugin
+  -- Autocompletion
+  { "onsails/lspkind-nvim" },
+  { "hrsh7th/nvim-cmp" },
+  { "hrsh7th/cmp-nvim-lsp" },
+  { "hrsh7th/cmp-buffer" },
+  { "saadparwaiz1/cmp_luasnip" }, -- Snippets source for nvim-cmp
+  { "L3MON4D3/LuaSnip" },         -- Snippets plugin
 
-	-- Quick Scope
-	{ "unblevable/quick-scope" },
+  -- Quick Scope
+  { "unblevable/quick-scope" },
 
-	-- Treesitter more highlight
-	{ "nvim-treesitter/nvim-treesitter", run = ":TSUpdate" },
+  -- Treesitter more highlight
+  { "nvim-treesitter/nvim-treesitter",     run = ":TSUpdate" },
 
-	-- Show indent line
-	{ "lukas-reineke/indent-blankline.nvim", dependencies = "tjdevries/colorbuddy.nvim" },
+  -- Show indent line
+  { "lukas-reineke/indent-blankline.nvim", dependencies = "tjdevries/colorbuddy.nvim" },
 
-	-- Fuzzy finder
-	{
-		"nvim-telescope/telescope.nvim",
-		version = "0.1.4",
-		dependencies = "nvim-lua/plenary.nvim",
-	},
-	{
-		"nvim-telescope/telescope-fzf-native.nvim",
-		build = function(plugin)
-			local obj = vim.system({ "cmake", "-S.", "-Bbuild", "-DCMAKE_BUILD_TYPE=Release" }, { cwd = plugin.dir })
-				:wait()
-			if obj.code ~= 0 then
-				error(obj.stderr)
-			end
-			obj = vim.system({ "cmake", "--build", "build", "--config", "Release" }, { cwd = plugin.dir }):wait()
-			if obj.code ~= 0 then
-				error(obj.stderr)
-			end
-			obj = vim.system({ "cmake", "--install", "build", "--prefix", "build" }, { cwd = plugin.dir }):wait()
-			if obj.code ~= 0 then
-				error(obj.stderr)
-			end
-		end,
-	},
-	{ "nvim-telescope/telescope-file-browser.nvim" },
-	{ "smartpde/telescope-recent-files" },
+  -- Fuzzy finder
+  {
+    "nvim-telescope/telescope.nvim",
+    version = "0.1.4",
+    dependencies = "nvim-lua/plenary.nvim",
+  },
+  {
+    "nvim-telescope/telescope-fzf-native.nvim",
+    build = function(plugin)
+      local do_build = function()
+        local obj = vim.system({ "cmake", "-S.", "-Bbuild", "-DCMAKE_BUILD_TYPE=Release" }, { cwd = plugin.dir })
+            :wait()
+        if obj.code ~= 0 then
+          error(obj.stderr)
+        end
+        obj = vim.system({ "cmake", "--build", "build", "--config", "Release" }, { cwd = plugin.dir }):wait()
+        if obj.code ~= 0 then
+          error(obj.stderr)
+        end
+        obj = vim.system({ "cmake", "--install", "build", "--prefix", "build" }, { cwd = plugin.dir }):wait()
+        if obj.code ~= 0 then
+          error(obj.stderr)
+        end
+      end
+      if vim.fn.has('mac') == 1 then
+        do_build()
+      elseif vim.fn.has('win32') == 1 then
+        local obj = vim.system({ "cmake", "-S.", "-Bbuild", "-DCMAKE_BUILD_TYPE=Release" }, { cwd = plugin.dir }):wait()
+        if obj.code ~= 0 then
+          error(obj.stderr)
+        end
+      elseif vim.fn.has('unix') == 1 then
+        do_build()
+      end
+    end,
+  },
+  { "nvim-telescope/telescope-file-browser.nvim" },
+  { "smartpde/telescope-recent-files" },
 
-	-- Scrollview
-	{ "dstein64/nvim-scrollview" },
+  -- Scrollview
+  { "dstein64/nvim-scrollview" },
 }
