af5edd40b983ffa339bd9c4b327547d6f4751e16
Jonas Gao <gaopinsong@live.cn>
2025-06-19 21:25:12 +0800

    Update build_telescope_fzf_native.ps1，add CMAKE_POLICY_VERSION_MINIMUM

--- lib/build_telescope_fzf_native.ps1
+++ lib/build_telescope_fzf_native.ps1
@@ -3,7 +3,7 @@ $LOG = "$(Get-Location)/build_telescope_fzf_native.log"
 $WDIR = $args[0]
 Set-Location $WDIR
 Write-Output "PWD: $(Get-Location)" > $LOG
-cmake "-S." "-Bbuild" "-DCMAKE_BUILD_TYPE=Release" >> $LOG && cmake "--build" build "--config" Release >> $LOG
+cmake "-S." "-Bbuild" "-DCMAKE_POLICY_VERSION_MINIMUM=3.5" "-DCMAKE_BUILD_TYPE=Release" >> $LOG && cmake "--build" build "--config" Release >> $LOG
 if (Test-Path "$WDIR\build\Release\libfzf.dll" -PathType Leaf)
 {
 	Write-Output "Moving libfzf.dll" > $LOG
